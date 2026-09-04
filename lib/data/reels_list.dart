c3316dcdeccb00fbed8084073d06c743dd4a1a4a
Aumesha <aumesha27@gmail.com>
2026-09-04 21:16:00 +0530

    Add ReelItem class and reelsList constant

---
+++ lib/data/reels_list.dart
@@ -0,0 +1,11 @@
+class ReelItem {
+  final String title;
+  final String url;
+  const ReelItem({required this.title, required this.url});
+}
+
+const reelsList = [
+  ReelItem(title: 'Top Govt Jobs Updates', url: 'https://www.instagram.com/'),
+  ReelItem(title: 'Resume Preparation Tips', url: 'https://www.instagram.com/'),
+  ReelItem(title: 'Interview Success Formula', url: 'https://www.instagram.com/'),
+];
