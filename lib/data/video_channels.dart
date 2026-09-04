a6393005869ff6df4da324cbdf54f3d668e71750
Aumesha <aumesha27@gmail.com>
2026-09-04 21:15:17 +0530

    Create video_channels.dart

---
+++ lib/data/video_channels.dart
@@ -0,0 +1,11 @@
+class VideoChannel {
+  final String name;
+  final String channelId;
+  const VideoChannel({required this.name, required this.channelId});
+}
+
+const videoChannels = [
+  VideoChannel(name: 'Unacademy Govt Exams', channelId: 'UCW4I31502fLEL4d633mB3DQ'),
+  VideoChannel(name: 'Adda247 Live Jobs', channelId: 'UC_096A3p9BscPj7xU5zL2TQ'),
+  VideoChannel(name: 'Testbook Govt Prep', channelId: 'UC23S948SsoC6C3I_vQ46nUQ'),
+];
